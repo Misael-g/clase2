4b36cb931d31c70d3fdbbd634567fce55dd0eff4
Misael-g <misael.garcia@epn.edu.ecc>
2024-10-10 07:58:58 +0200

    Initial commit

---
+++ src/Libro.java
@@ -0,0 +1,22 @@
+public class Libro {
+    //Atributos
+    String titulo;
+    String autor;
+    String editorial;
+    int paginas;
+
+
+    //métodos
+    //una clase tiene que terner el metodo constructor siempre
+    //Constructor
+    public Libro(String titulo, String autor, String editorial, int paginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editorial = editorial;
+        this.paginas = paginas;
+
+    }
+
+
+
+}
